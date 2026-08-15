4c2191e8007396c09e0edf893a406caba3335b0d
ogueell <miguelpalmeira.dev@gmail.com>
2026-08-14 21:57:11 -0300

    Adiciona interatividade com Scanner: listagem de produtos, escolha do usuario e calculo do total

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
 
+        List<Produto> produtos = new ArrayList<>();
+
+        Produto teclado = new Produto();
+        teclado.setNome("Teclado");
+        teclado.setPreco(150.00);
+        teclado.setQuantidade(10);
+
+        Produto mouse = new Produto();
+        mouse.setNome("Mouse");
+        mouse.setPreco(50.00);
+        mouse.setQuantidade(5);
+
+        Produto monitor = new Produto();
+        monitor.setNome("Monitor");
+        monitor.setPreco(300.00);
+        monitor.setQuantidade(10);
+
+        produtos.add(teclado);
+        produtos.add(mouse);
+        produtos.add(monitor);
+
+        Carrinho carrinho = new Carrinho();
+
+        System.out.println("=== Produtos disponíveis ===");
+        for (int i = 0; i < produtos.size(); i++) {
+            Produto produtoAtual = produtos.get(i);
+            System.out.println((i + 1) + " - " + produtoAtual.getNome() + " - R$" + produtoAtual.getPreco());
+
+        }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha o número do produto: ");
+        int escolha = scanner.nextInt();
+
+        System.out.println("Quantidade desejada: ");
+        int quantidade = scanner.nextInt();
+
+        Produto produtoEscolhido = produtos.get(escolha - 1);
+
+        ItemCarrinho itemCarrinho = new ItemCarrinho();
+        itemCarrinho.setProduto(produtoEscolhido);
+        itemCarrinho.setQuantidade(quantidade);
+
+        carrinho.adicionarItem(itemCarrinho);
+
+        System.out.println("Total: " + carrinho.calcularTotal());
     }
 }
